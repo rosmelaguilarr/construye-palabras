f4d7c84a6c6497c8d66e0e475319c2609f154126
Rosmel <rosmeltics@gmail.com>
2022-02-09 23:16:28 -0500

    Versión 1.1

--- js/main.js
+++ js/main.js
@@ -16,116 +16,122 @@ const d = document,
           },
      ];
 
-let correct = 0,
-     total = 0,
-     time = 60;
-
-const getLetters = (indexStockArray) => {
-     const arraySelect = stockArray[indexStockArray].content;
-
-     let lengthLetters = arraySelect.length,
-          indexRandom = Math.floor(Math.random() * lengthLetters),
-          newArray = arraySelect[indexRandom].split("");
-     return newArray;
-}
-let letter = "";
-const printLetters = (indexStockArray = 0) => {
-     let arrayDroppable = getLetters(indexStockArray),
-          array1 = [...arrayDroppable].sort(),
-          arrayABC = [...getLetters(indexStockArray).reverse(), ...array1, ...getLetters(indexStockArray).sort()];
-
-     letter = arrayDroppable.join("");
-     $img.setAttribute("src", `img/${stockArray[indexStockArray].name}/${letter}.png`);
-
-     arrayABC.forEach(el => {
-          const $draggableElement = d.createElement("div")
-          $draggableElement.textContent = el.toLocaleUpperCase(),
-               $draggableElement.classList.add("draggable");
-          $draggableElement.setAttribute("draggable", "true");
-          $draggableElement.setAttribute("data-id", el);
-          $draggableFragment.appendChild($draggableElement);
-     });
-
-     arrayDroppable.forEach(el => {
-          const $droppableElement = d.createElement("div");
-          $droppableElement.classList.add("droppable");
-          $droppableElement.setAttribute("data-id", el);
-          $droppableFragment.appendChild($droppableElement);
-     });
-
-     $draggableContainer.appendChild($draggableFragment);
-     $droppableContainer.appendChild($droppableFragment);
-}
-
-printLetters(); // Pasar el indice (default: 0) del array Ejm: 0, 1, ...
-
 const $correctSpan = d.querySelector(".correct"),
      $totalSpan = d.querySelector(".total"),
-     $timeDiv = d.querySelector(".time__number");
+     $timeDiv = d.querySelector(".time__number"),
+     $mensageContainer = d.querySelector(".message-container"),
+     $messageTitle = d.querySelector(".message__title"),
+     $startBtn = d.querySelector(".message__start-btn");
 
-d.addEventListener("DOMContentLoaded", e => {
-     const $draggableElements = d.querySelectorAll(".draggable"),
-          $droppableElements = d.querySelectorAll(".droppable");
+let correct = 0,
+     total = 0,
+     time = 60,
+     letter = "";
 
-     const dragStart = (e) => {
-          e.dataTransfer.setData("text", e.target.dataset.id);
+const startGame = () => {
+     // Obtener letra para el juego del stockArray
+     const getLetters = (indexStockArray) => {
+          const arraySelect = stockArray[indexStockArray].content;
+
+          let lengthLetters = arraySelect.length,
+               indexRandom = Math.floor(Math.random() * lengthLetters),
+               newArray = arraySelect[indexRandom].split("");
+          return newArray;
      }
-     const dragOver = (e) => {
-          e.preventDefault();
-          e.target.classList.add("droppable-hover")
+     // Imprimir imagen, letras draggables y droppables
+     const printLetters = (indexStockArray = 0) => {
+          let arrayDroppable = getLetters(indexStockArray),
+               array1 = [...arrayDroppable].sort(),
+               arrayABC = [...getLetters(indexStockArray).reverse(), ...array1, ...getLetters(indexStockArray).sort()];
+
+          letter = arrayDroppable.join("");
+          $img.setAttribute("src", `img/${stockArray[indexStockArray].name}/${letter}.png`);
+
+          arrayABC.forEach(el => {
+               const $draggableElement = d.createElement("div")
+               $draggableElement.textContent = el.toLocaleUpperCase(),
+                    $draggableElement.classList.add("draggable");
+               $draggableElement.setAttribute("draggable", "true");
+               $draggableElement.setAttribute("data-id", el);
+               $draggableFragment.appendChild($draggableElement);
+          });
+
+          arrayDroppable.forEach(el => {
+               const $droppableElement = d.createElement("div");
+               $droppableElement.classList.add("droppable");
+               $droppableElement.setAttribute("data-id", el);
+               $droppableFragment.appendChild($droppableElement);
+          });
+
+          $draggableContainer.appendChild($draggableFragment);
+          $droppableContainer.appendChild($droppableFragment);
      }
-     const dragLeave = (e) => {
-          e.target.classList.remove("droppable-hover")
-     }
-     const drop = (e) => {
-          const $draggableElementId = e.dataTransfer.getData("text"),
-               $droppableElementId = e.target.dataset.id,
-               $draggableElement = d.querySelector(`.main__abc [data-id = ${$draggableElementId}]`)
-          e.target.classList.remove("droppable-hover");
+     printLetters();
 
-          let dropped = e.target.classList.contains("droppable");
+     // Añadir eventos de drag y drop a letras
+     const dragDrop = () => {
+          const $draggableElements = d.querySelectorAll(".draggable"),
+               $droppableElements = d.querySelectorAll(".droppable");
 
-          if (($draggableElementId === $droppableElementId) && dropped) {
-               $draggableElement.setAttribute("draggable", "false");
-               e.target.textContent = "";
-               e.target.classList.add("dropped-ok");
-               e.target.append($draggableElement);
-               correct++;
-
-          } else if (dropped) {
-               e.target.append($draggableElement);
-               e.target.classList.add("dropped-error");
-               total++;
+          const dragStart = (e) => {
+               e.dataTransfer.setData("text", e.target.dataset.id);
           }
-          $correctSpan.innerHTML = correct;
-          $totalSpan.innerHTML = total;
-     }
+          const dragOver = (e) => {
+               e.preventDefault();
+               e.target.classList.add("droppable-hover")
+          }
+          const dragLeave = (e) => {
+               e.target.classList.remove("droppable-hover")
+          }
+          const drop = (e) => {
+               const $draggableElementId = e.dataTransfer.getData("text"),
+                    $droppableElementId = e.target.dataset.id,
+                    $draggableElement = d.querySelector(`.main__abc [data-id = ${$draggableElementId}]`)
+               e.target.classList.remove("droppable-hover");
 
-     $draggableElements.forEach(el => {
-          el.addEventListener("dragstart", dragStart);
-     });
-     $droppableElements.forEach(el => {
-          el.addEventListener("dragover", dragOver);
-          el.addEventListener("dragleave", dragLeave);
-          el.addEventListener("drop", drop);
-     });
+               let dropped = e.target.classList.contains("droppable");
 
-     d.addEventListener("click", e => {
-          if (e.target.matches(".droppable *")) {
-               if (e.target.matches("[draggable='true']")) {
-                    e.target.parentElement.classList.remove("dropped-error");
-                    $draggableContainer.appendChild(e.target);
+               if (($draggableElementId === $droppableElementId) && dropped) {
+                    $draggableElement.setAttribute("draggable", "false");
+                    e.target.textContent = "";
+                    e.target.classList.add("dropped-ok");
+                    e.target.append($draggableElement);
+                    correct++;
+
+               } else if (dropped) {
+                    e.target.append($draggableElement);
+                    e.target.classList.add("dropped-error");
+                    total++;
                }
+               $correctSpan.innerHTML = correct;
+               $totalSpan.innerHTML = total;
           }
-     });
 
-     const $mensageContainer = d.querySelector(".message-container"),
-          $messageTitle = d.querySelector(".message__title"),
-          $aciertos = d.querySelector(".aciertos__span"),
-          $fallos = d.querySelector(".fallos__span"),
-          $startBtn = d.querySelector(".message__start-btn");
+          $draggableElements.forEach(el => {
+               el.addEventListener("dragstart", dragStart);
+          });
+          $droppableElements.forEach(el => {
+               el.addEventListener("dragover", dragOver);
+               el.addEventListener("dragleave", dragLeave);
+               el.addEventListener("drop", drop);
+          });
 
+          d.addEventListener("click", e => {
+               if (e.target.matches(".droppable *")) {
+                    if (e.target.matches("[draggable='true']")) {
+                         e.target.parentElement.classList.remove("dropped-error");
+                         $draggableContainer.appendChild(e.target);
+                    }
+               }
+          });
+     }
+     dragDrop();
+
+     // Imprimir mensaje de Ganador o Perdedor
      const printMessage = (clase, message) => {
+          const $aciertos = d.querySelector(".aciertos__span"),
+               $fallos = d.querySelector(".fallos__span");
+
           $mensageContainer.style.display = "flex";
           $messageTitle.classList.add(clase);
           $messageTitle.innerHTML = message;
@@ -145,9 +151,25 @@ d.addEventListener("DOMContentLoaded", e => {
                printMessage("loser", "¡Tiempo agotado!");
           }
      }, 1000);
+} // End game
 
-     $startBtn.addEventListener("click", e => {
-          location.reload();
-     });
+startGame();
+
+$startBtn.addEventListener("click", e => {
+     $mensageContainer.style.display = "none";
+     $droppableContainer.innerHTML = "";
+     $draggableContainer.innerHTML = "";
+     $messageTitle.classList.remove("winner", "loser");
+     $timeDiv.innerHTML = 60;
+     $correctSpan.innerHTML = 0;
+     $totalSpan.innerHTML = 0;
+     correct = 0;
+     total = 0;
+     time = 60;
+     startGame();
 });
+
+
+
+
 
